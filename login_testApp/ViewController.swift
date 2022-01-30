c72e6276b9ed2bd9e0e47a03b2f32c2254c245f6
Maxim Mitin <maximus405@gmail.com>
2022-01-31 00:12:38 +0300

    main config for FB integration

--- login_testApp/ViewController.swift
+++ login_testApp/ViewController.swift
@@ -6,14 +6,48 @@
 //
 
 import UIKit
+import FBSDKLoginKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, LoginButtonDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        if let token = AccessToken.current, !token.isExpired {
+            let token = token.tokenString
+            
+            let reuqest = FBSDKLoginKit.GraphRequest(graphPath: "me",
+                                                     parameters: ["fields":"email, name"],
+                                                     tokenString: token, version: nil,
+                                                     httpMethod: .get)
+            
+            reuqest.start(completion: {connection,results,error in
+                print("\(results)")
+            })
+        } else {
+            let loginButton = FBLoginButton()
+            loginButton.delegate = self
+            loginButton.permissions = ["public_profile", "email, name"]
+            view.addSubview(loginButton)
+        }
+        
     }
 
+    func loginButton(_ loginButton: FBLoginButton, didCompleteWith result: LoginManagerLoginResult?, error: Error?) {
+        let token = result?.token?.tokenString
+        
+        let reuqest = FBSDKLoginKit.GraphRequest(graphPath: "me",
+                                                 parameters: ["fields":"email"],
+                                                 tokenString: token, version: nil,
+                                                 httpMethod: .get)
+        
+        reuqest.start(completion: {connection,results,error in
+            print("\(results)")
+        })
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBLoginButton) {
+        
+    }
 
 }
 
